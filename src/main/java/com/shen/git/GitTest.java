f1ac928122867f17f9bc770f7f07c05fd7d51c1a
shenzhen <shenzhen@shen.com>
2021-06-02 17:10:42 +0800

    second commit

--- src/main/java/com/shen/git/GitTest.java
+++ src/main/java/com/shen/git/GitTest.java
@@ -7,5 +7,6 @@ package com.shen.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!2");
     }
 }
