f70fce1d5cde5a0d8e640bb84fb059ef20ab15c5
shenzhen <shenzhen@shen.com>
2021-06-02 17:07:15 +0800

    init commit

---
+++ src/main/java/com/shen/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.shen.git;
+
+/**
+ * @author sz
+ * @date 2021/6/2 - 17:02
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
