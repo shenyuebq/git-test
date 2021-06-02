3d24ce31e94b640ce7a339ae5147fe192a5033c1
shenzhen <shenzhen@shen.com>
2021-06-02 17:37:48 +0800

    M test2

--- src/main/java/com/shen/git/GitTest.java
+++ src/main/java/com/shen/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git!3");
         System.out.println("hello git!4");
         System.out.println("master test");
+        System.out.println("master test2");
     }
 }
