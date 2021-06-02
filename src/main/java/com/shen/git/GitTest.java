25a541476d70adb93f6778c9ed5dbd3226125d99
shenzhen <shenzhen@shen.com>
2021-06-02 19:02:12 +0800

    push test11

--- src/main/java/com/shen/git/GitTest.java
+++ src/main/java/com/shen/git/GitTest.java
@@ -15,5 +15,7 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("hot-fix test2");
         System.out.println("push test");
+        System.out.println("push test");
+        System.out.println("push test");
     }
 }
