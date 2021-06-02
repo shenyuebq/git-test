c97487ceafce8ba572d04c662a59f3a423b36078
shenzhen <shenzhen@shen.com>
2021-06-02 17:37:08 +0800

    F test2

--- src/main/java/com/shen/git/GitTest.java
+++ src/main/java/com/shen/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git!3");
         System.out.println("hello git!4");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test2");
     }
 }
